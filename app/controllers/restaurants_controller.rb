7b10ba937f9bafecafb3e9c6dc02ce6089f889a7
SebastienPires <pires.sebastien@gmail.com>
2015-03-30 14:21:22 +0100

    Added create a restaurant

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,5 +1,20 @@
 class RestaurantsController < ApplicationController
+
   def index
-    p 'Hello from MA'
+    @restaurants = Restaurant.all
   end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.create(restaurant_params)
+    redirect_to '/restaurants'
+  end
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name)
+  end
+
 end
