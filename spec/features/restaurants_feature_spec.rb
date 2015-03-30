0d49b2dc3069411fc966fe5d349c4e4381ab27c0
Thomas Strothjohann <thomas@strothjohann.net>
2015-03-30 15:29:58 +0100

    User can edit restaurants now

--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -42,5 +42,17 @@ feature 'restaurants' do
     end
   end
 
+  context 'editing restaurants' do
+    before {Restaurant.create name: 'KFC'}
+    scenario 'let a user edit a restaurant' do
+     visit '/restaurants'
+     click_link 'Edit KFC'
+     fill_in 'Name', with: 'Kentucky Fried Chicken'
+     click_button 'Update Restaurant'
+     expect(page).to have_content 'Kentucky Fried Chicken'
+     expect(current_path).to eq '/restaurants'
+    end
+  end
+
 
 end
