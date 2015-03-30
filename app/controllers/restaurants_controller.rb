034e697562e781bbcf63c5f515742bcf7b4db277
SebastienPires <pires.sebastien@gmail.com>
2015-03-30 14:47:34 +0100

    Can edit restaurants

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -17,4 +17,18 @@ class RestaurantsController < ApplicationController
     params.require(:restaurant).permit(:name)
   end
 
+  def show
+  @restaurant = Restaurant.find(params[:id])
+  end
+
+  def edit
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def update
+    @restaurant = Restaurant.find(params[:id])
+    @restaurant.update(restaurant_params)
+    redirect_to '/restaurants'
+  end
+
 end
--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -27,7 +27,9 @@ feature 'restaurants' do
   end
 
   context 'creating restaurants' do
+
     scenario 'prompts user to fill out a form, then displays the new restaurant' do
+
       visit '/restaurants'
       click_link 'Add a restaurant'
       fill_in 'Name', with: 'KFC'
@@ -36,4 +38,32 @@ feature 'restaurants' do
       expect(current_path).to eq '/restaurants'
     end
   end
+
+  context 'viewing restaurants' do
+
+  let!(:kfc){Restaurant.create(name:'KFC')}
+
+  scenario 'lets a user view a restaurant' do
+   visit '/restaurants'
+   click_link 'KFC'
+   expect(page).to have_content 'KFC'
+   expect(current_path).to eq "/restaurants/#{kfc.id}"
+  end
+
+end
+
+context 'editing restaurants' do
+
+  before {Restaurant.create name: 'KFC'}
+
+  scenario 'let a user edit a restaurant' do
+   visit '/restaurants'
+   click_link 'Edit KFC'
+   fill_in 'Name', with: 'Kentucky Fried Chicken'
+   click_button 'Update Restaurant'
+   expect(page).to have_content 'Kentucky Fried Chicken'
+   expect(current_path).to eq '/restaurants'
+  end
+
+end
 end
