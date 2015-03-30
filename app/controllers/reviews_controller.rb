bdfab7bf7c8b88d8af9bf3e287f66946b04af33a
SebastienPires <pires.sebastien@gmail.com>
2015-03-30 16:56:49 +0100

    Linked reviews to restaurants and linked to db

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,5 @@
 class ReviewsController < ApplicationController
-  
+
   def new
     @restaurant = Restaurant.find(params[:restaurant_id])
     @review = Review.new
@@ -8,9 +8,13 @@ class ReviewsController < ApplicationController
   def create
     @restaurant = Restaurant.find(params[:restaurant_id])
     @restaurant.reviews.create(review_params)
+    redirect_to restaurants_path
   end
 
   def review_params
     params.require(:review).permit(:thoughts, :rating)
   end
 end
+
+
+
