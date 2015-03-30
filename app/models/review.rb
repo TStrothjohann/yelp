7652d64be86f2208e7a9148177a2807a613c661f
Thomas Strothjohann <thomas@strothjohann.net>
2015-03-30 20:39:22 +0100

    Added a validation for review rating equal or less than 5

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,4 @@
 class Review < ActiveRecord::Base
   belongs_to :restaurant
+  validates :rating, numericality: { less_than_or_equal_to: 5 }
 end
---
+++ spec/models/review_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+
+describe Review, :type => :model do
+  before {Restaurant.create name: 'KFC'}
+  it 'is invalid if the rating is more than 5' do
+    review = Restaurant.find_by(name: 'KFC').reviews.create(rating: 6)
+    expect(review).to have(1).error_on(:rating)
+  end
+end
